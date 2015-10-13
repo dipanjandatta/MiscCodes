dd0b62004cde9da5ef5092ad5d48d4440d66d0b5
Dipanjan Datta <dipanjan19.05@gmail.com>
2015-10-13 16:39:47 +0530

    Update FivePtSum.R

--- FivePtSum.R
+++ FivePtSum.R
@@ -1,48 +1,14 @@
-rm(list=ls())
-
-Sector_Summary= function(x){
-
-  #Calculates the Five point Summary
-  x= na.omit(x)
-  Sector_Number= nrow(x)
-  Sector_Minimum= min(x)
-  Sector_1stQuartile= quantile(x,0.25)
-  Sector_Median= quantile(x,0.5)
-  Sector_3rdQuartile= quantile(x,0.75)
-  Sector_Maximum= max(x)
-  data.frame(Sector_Number,Sector_Minimum,Sector_1stQuartile,Sector_Median,
-             Sector_3rdQuartile,Sector_Maximum)
-  
-}
-
-
-Index_Summary= function(x){
+My_Summary= function(x){
   
   #Calculates the Five point Summary
   x= na.omit(x)
-  Index_Number= nrow(x)
-  Index_Minimum= min(x)
-  Index_1stQuartile= quantile(x,0.25)
-  Index_Median= quantile(x,0.5)
-  Index_3rdQuartile= quantile(x,0.75)
-  Index_Maximum= max(x)
-  data.frame(Index_Number,Index_Minimum,Index_1stQuartile,Index_Median,
-             Index_3rdQuartile,Index_Maximum)
+  My_Number= length(x)
+  My_Minimum= min(x)
+  My_1stQuartile= quantile(x,0.25)
+  My_Median= quantile(x,0.5)
+  My_3rdQuartile= quantile(x,0.75)
+  My_Maximum= max(x)
+  data.frame(My_Number,My_Minimum,My_1stQuartile,My_Median,
+             My_3rdQuartile,My_Maximum)
   
 }
-
-BSE_Summary= function(x){
-  
-  #Calculates the Five point Summary
-  x= na.omit(x)
-  BSE_Number= nrow(x)
-  BSE_Minimum= min(x)
-  BSE_1stQuartile= quantile(x,0.25)
-  BSE_Median= quantile(x,0.5)
-  BSE_3rdQuartile= quantile(x,0.75)
-  BSE_Maximum= max(x)
-  data.frame(BSE_Number,BSE_Minimum,BSE_1stQuartile,BSE_Median,
-             BSE_3rdQuartile,BSE_Maximum)
-  
-}
-
